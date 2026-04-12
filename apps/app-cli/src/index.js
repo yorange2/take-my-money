a8b360a758e0fd983ca941d24ea628f27f16a72a
yorange2 <yuan8967@gmail.com>
2026-04-12 12:07:47 +0800

    Remove demo apps (app-cli and app-web)

--- apps/app-cli/src/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { greet, add } from '@take-my-money/shared-lib';
-
-console.log(greet('World'));
-console.log(`2 + 3 = ${add(2, 3)}`);
--- apps/app-web/src/index.js
+++
@@ -1,4 +0,0 @@
-import { greet } from '@take-my-money/shared-lib';
-
-console.log('Web app started');
-console.log(greet('Web User'));
